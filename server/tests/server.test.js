e040520d1cf1eb84db802731270c7bc708f23bb5
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-07-14 11:02:00 -0700

    Add tests for GET /todos/:id routes

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,8 @@
 const expect = require('expect');
 const request = require('supertest');
+const {
+    ObjectId
+} = require("mongodb");
 
 const {
     app
@@ -7,8 +10,10 @@ const {
 const Todo = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectId(),
     text: "First test todo"
 }, {
+    _id: new ObjectId(),
     text: 'Second text todo'
 }];
 
@@ -66,5 +71,20 @@ describe("GET /todos", () => {
         request(app).get('/todos').expect(200).expect(res => {
             expect(res.body.todos.length).toBe(2);
         }).end(done);
+    });
+});
+
+describe("GET /todos/:id", () => {
+    it("should return todo doc", done => {
+        request(app).get(`/todos/${todos[0]._id.toHexString()}`).expect(200).expect(res => {
+            expect(res.body.todo.text).toBe(todos[0].text);
+        }).end(done);
+    });
+    it("should return 404 if todo not found", done => {
+        var hexId = new ObjectId().toHexString();
+        request(app).get(`/todos/${(new ObjectId()).toHexString()}`).expect(404).end(done);
+    });
+    it("should return 404 for non-object ids", done => {
+        request(app).get("/todos/123abc").expect(404).end(done);
     });
 });
