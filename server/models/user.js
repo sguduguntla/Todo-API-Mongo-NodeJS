80556f2658d89278c74abaa4995912f9805a1748
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-07-14 12:26:29 -0700

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require("jsonwebtoken");
+const _ = require('lodash');
 
 const Schema = mongoose.Schema;
 
@@ -7,9 +10,56 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function() {
+    var user = this;
+
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = "auth";
+    var token = jwt.sign({
+        _id: user._id.toHexString(),
+        access: access
+    }, 'abc123').toString();
+
+    user.tokens.push({
+        access: access,
+        token: token
+    });
+
+    return user.save().then(() => {
+        return token;
+    });
+
+};
 
 const User = mongoose.model('user', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -111,6 +111,19 @@ app.patch("/todos/:id", (req, res, next) => {
     })
 });
 
+// POST /users
+app.post("/users", (req, res, next) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then(token => {
+        res.header('x-auth', token).send(user);
+    }).catch(e => {
+        res.status(400).send();
+    });
+})
 
 app.listen(PORT, () => {
     console.log(`Started on port ${PORT}`);
