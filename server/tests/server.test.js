6b1535041f0dc757de6f07abb2ea3164b321c7df
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-07-14 11:19:14 -0700

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -88,3 +88,30 @@ describe("GET /todos/:id", () => {
         request(app).get("/todos/123abc").expect(400).end(done);
     });
 });
+
+describe("DELETE /todos/:id", () => {
+    it("should remove a todo", done => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app).delete(`/todos/${hexId}`).expect(200).expect(res => {
+            expect(res.body.todo._id).toBe(hexId);
+        }).end((err, res) => {
+            if (err) {
+                return done(err);
+            } else {
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch(e => done(e));
+            }
+        });
+
+    });
+    it("should return 404 if todo not found", done => {
+        var hexId = new ObjectId().toHexString();
+        request(app).delete(`/todos/${(new ObjectId()).toHexString()}`).expect(404).end(done);
+    });
+    it("should return 400 if object id is invalid", done => {
+        request(app).delete("/todos/123abc").expect(400).end(done);
+    });
+});
